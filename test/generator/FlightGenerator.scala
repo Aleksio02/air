b8c6156e8698a13caf96151aef2f7870f5e996b1
Aleksio02 <oleks1o@yandex.ru>
2025-03-04 17:09:12 +0300

    [master] Переписаны генераторы для сущностей полетов

--- test/generator/FlightGenerator.scala
+++ test/generator/FlightGenerator.scala
@@ -9,10 +9,13 @@ import scala.util.Random
 object AircraftGenerator {
   val gen: Gen[Aircraft] = {
     for {
-      i <- Gen.chooseNum(0, 100)
+      c <- Gen.hexStr.map(AircraftCode(_))
+      m <- Gen.option(Gen.alphaUpperStr)
+      r <- Gen.chooseNum(1000f, 2300f)
+      cl <- Gen.choose(1, 5)
+      v <- Gen.const(500)
     } yield {
-      Aircraft(AircraftCode(s"Aircraft ${new Random().between(1, 101)}"), Option("Model T"),
-        new Random().between(1000f, 2300f + i), new Random().between(1, 5), 500)
+      Aircraft(c, m, r, cl, v)
     }
   }
 }
@@ -20,10 +23,16 @@ object AircraftGenerator {
 object AirportGenerator {
   val gen: Gen[Airport] = {
     for {
-      i <- Gen.chooseNum(0, 100)
+      c <- Gen.alphaNumStr.map(AirportCode(_))
+      n <- Gen.identifier
+      ci <- Gen.alphaLowerStr
+      tz <- Gen.numStr
+      co <- Gen.option(Gen.asciiPrintableStr)
+      isoc <- Gen.option(Gen.const(""))
+      isor <- Gen.option(Gen.const(""))
+      intnl <- Gen.const(true)
     } yield {
-      Airport(AirportCode(s"Airport ${i}"), s"some airport name", "Simferopol", "GMT+3",
-        Option("Europe"), Option(""), Option(""), true,
+      Airport(c, n, ci, tz, co, isoc, isor, intnl,
         Option(LocalDateTime.parse("2025-01-01 00:00:00", Commons.formatter)))
     }
   }
@@ -32,13 +41,17 @@ object AirportGenerator {
 object FlightGenerator {
   val gen: Gen[Flight] = {
     for {
-      i <- Gen.chooseNum(0, 100)
+      id <- Gen.chooseNum(1, 10).map(FlightId(_))
+      no <- Gen.alphaNumStr
+      apc1 <- Gen.asciiPrintableStr.map(AirportCode(_))
+      apc2 <- Gen.asciiPrintableStr.map(AirportCode(_))
+      st <- Gen.oneOf("Successfull", "In progress", "Not started", "Cancelled")
+      acc <- Gen.hexStr.map(AircraftCode(_))
     } yield {
-      Flight(FlightId(i), s"flight no - ${i}",
+      Flight(id, no,
         LocalDateTime.parse("2025-02-07 00:00:00", Commons.formatter),
         LocalDateTime.parse("2025-02-07 04:00:00", Commons.formatter),
-        AirportCode(s"Airport ${i}"), AirportCode(s"Airport ${(i + 10) % 100}"),
-        "Successful", AircraftCode(s"Aircraft ${new Random().between(1, 101)}"),
+        apc1, apc2, st, acc,
         Option(LocalDateTime.parse("2025-02-07 00:06:10", Commons.formatter)),
         Option(LocalDateTime.parse("2025-02-07 04:10:57", Commons.formatter)),
         Option(LocalDateTime.parse("2025-01-01 00:00:00", Commons.formatter)))
@@ -49,10 +62,14 @@ object FlightGenerator {
 object BoardingPassGenerator {
   val gen: Gen[BoardingPass] = {
     for {
-      i <- Gen.chooseNum(0, 100)
+      id <- Gen.chooseNum(1, 10).map(BoardingPassId(_))
+      pId <- Gen.option(Gen.choose(11, 19).map(PassengerId(_)))
+      blId <- Gen.option(Gen.choose(11, 19).map(BookingLegId(_)))
+      s <- Gen.option(Gen.identifier)
+      pc <- Gen.option(Gen.const(false))
     } yield {
-      BoardingPass(BoardingPassId(i), Option(PassengerId(i)), Option(BookingLegId(i)), Option(s"Seat ${i}"),
-        Option(LocalDateTime.parse("2025-02-06 23:49:00", Commons.formatter)), Option(new Random().nextBoolean()),
+      BoardingPass(id, pId, blId, s,
+        Option(LocalDateTime.parse("2025-02-06 23:49:00", Commons.formatter)), pc,
         Option(LocalDateTime.parse("2025-01-01 00:00:00", Commons.formatter)))
     }
   }
